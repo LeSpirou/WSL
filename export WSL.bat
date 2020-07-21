b0f1ed15a7a71c973e6f494cf71f7dda7ab90622
LeSpirou <lespirou@convidca.ch>
2020-07-21 19:57:21 +0200

    Update export WSL.bat

--- export WSL.bat
+++ export WSL.bat
@@ -1,7 +1,8 @@
 @echo off
 if [%~1] == [] (set distro=Debian) else set distro=%~1
+wsl -t "%distro%"
 set dt=%date:~8,2%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%
 set dt=%dt: =0%
-set filename=WSL-%distro%-%suffix%.tar
+set filename=WSL-%distro%-%dt%.tar
 wsl --export "%distro%" "%filename%"
 move /Y "%filename%" "C:\Users\%USERNAME%\Documents\WSL Backups\%filename%"
