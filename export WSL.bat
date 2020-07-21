eb7c9c1133384ad24a6af18629cf2b4cedf21eee
LeSpirou <lespirou@convidca.ch>
2020-07-21 19:44:28 +0200

    Add files via upload

---
+++ export WSL.bat
@@ -0,0 +1,7 @@
+@echo off
+if [%~1] == [] (set distro=Debian) else set distro=%~1
+set dt=%date:~8,2%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%
+set dt=%dt: =0%
+set filename=WSL-%distro%-%suffix%.tar
+wsl --export "%distro%" "%filename%"
+move /Y "%filename%" "C:\Users\%USERNAME%\Documents\WSL Backups\%filename%"
